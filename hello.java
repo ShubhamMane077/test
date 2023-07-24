73dedbf3d2f1cbf58d863eb194ae2fb8fc09ac86
ShubhamMane077 <140085529+ShubhamMane077@users.noreply.github.com>
2023-07-24 11:10:37 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ public class hello{
      
       System.out.println("Hello Git and Github..!"); 
       System.out.println("I have Learn so many things about github."); 
+        System.out.println("Hello Git and Github no need to make fun while learn"); 
   }
 }
